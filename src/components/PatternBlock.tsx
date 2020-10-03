e9cd4e58daa231e208dcb145bb4dcecdc6123e64
Stepan Snigur <snigur-stepan@mail.ru>
2020-10-03 18:02:06 +0300

    Add copy to clipboard function

--- src/components/PatternBlock.tsx
+++ src/components/PatternBlock.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 interface IPatternBlock {
   name: string,
   regExp: string
 }
 
+const fadeIn = keyframes`
+  from {
+    top: 0;
+    opacity: 0;
+  }
+  to {
+    top: -90%;
+    opacity: 1;
+  }
+`
 const PatternBlockWrapper = styled.div`
   width: 30%;
   margin-bottom: 3%;
@@ -27,19 +37,67 @@ const PatternBlockWrapper = styled.div`
     display: block;
     width: 85%;
     height: 40px;
+    padding: 0 20px;
     margin 20px auto 0 auto;
     border-radius: 20px;
     text-align: center;
     border: 0.5px solid #eee;
     outline: none;
+    cursor: pointer;
+  }
+`
+const CopyBlock = styled.div`
+  position: relative;
+
+  div {
+    display: none;
+    width: 100%;
+    height: 37px;
+    line-height: 40px;
+    position: absolute;
+    top: -90%;
+    opacity: 1;
+    text-align: center;
+    border: 1px solid #f5f5f5;
+    border-radius: 20px;
+    background: #eee;
+    user-select: none;
+    
+    &:before {
+      position: absolute;
+      content: '';
+      width: 15px;
+      height: 15px;
+      bottom: -7.5px;
+      left: 50%;
+      transform: translateX(-50%) rotate(45deg);
+      background: #eee;
+      border: 1px solid #eee;
+      border-top: none;
+      border-left: none;
+    }
+  }
+  &:hover {
+    div {
+      display: block;
+      animation: ${fadeIn} .5s;
+    }
   }
 `
 
 const PatternBlock: React.FC<IPatternBlock> = ({ name, regExp }) => {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(regExp)
+      .catch(e => console.log('Error in copy: ', e))
+  }
+
   return (
     <PatternBlockWrapper>
       <h5>{name}</h5>
-      <input type="text" readOnly={true} value={regExp}/>
+      <CopyBlock onClick={copyToClipboard}>
+        <div>Копировать в буфер обмена</div>
+        <input type="text" readOnly={true} value={regExp}/>
+      </CopyBlock>
       <input type="text" placeholder="Введите тестовое слово"/>
     </PatternBlockWrapper>
   )
---
+++ src/tests/PatternBlock.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import fireEvent from '@testing-library/user-event'
+import PatternBlock from '../components/PatternBlock'
+
+describe('PatternBlock', () => {
+  let PatternBlockComponent: any;
+  beforeEach(() => {
+    PatternBlockComponent = render(<PatternBlock name="test" regExp="test" />)
+  })
+
+  test('Test copy to clipboard', () => {
+    Object.defineProperty(window.navigator, 'clipboard', {
+      value: {
+        writeText: jest.fn(() => Promise.resolve(true))
+      }
+    })
+    const node = PatternBlockComponent.container.querySelector('div').querySelector('div')
+    fireEvent.click(node)
+    expect(window.navigator.clipboard.writeText).toHaveBeenCalled()
+  })
+})
